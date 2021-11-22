0f6d2177c1b0d70200f7c4d90cc90a589fe10781
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2021-11-22 16:15:52 +0100

    working on filtering the feautered artworks

--- JS/index.js
+++ JS/index.js
@@ -30,3 +30,44 @@ async function getHeroBannerData() {
   }
 }
 getHeroBannerData();
+
+async function getFeaturedArtworksData() {
+  try {
+    const repsonse = await fetch(
+      "https://makers-studio.herokuapp.com/Products/"
+    );
+    const data = await repsonse.json();
+    let featuredData = data;
+
+    featuredData.forEach((artwork) => {
+      let desiredArtwork = ${artwork.Title};
+
+      document.querySelector(".featured-products-artwork").innerHTML += `
+      <div class="featured-products-artwork-single">
+      <img src="${artwork.Image_url}" alt="an image of a painting">
+      <h3>${artwork.Title}</h3>
+      <h4>${artwork.Price}$</h4>
+      </div>
+        `;
+    });
+
+    /* ------------------------------------------ HideLoader ---------------------------------------------- */
+    const loaderContent = document.querySelector(".loader");
+
+    setTimeout(function () {
+      loaderContent.style.display = "none";
+      showMoreBtn.style.display = "block";
+    });
+    /* ----------------------------------------- /HideLoader ---------------------------------------------- */
+  } catch (error) {
+    document.querySelector(".alert").innerHTML += thisIsAnAlert(
+      "An error has occured",
+      "danger"
+    );
+  } finally {
+    setTimeout(function () {
+      document.querySelector(".alert").innerHTML = "";
+    }, 3000);
+  }
+}
+getFeaturedArtworksData();
