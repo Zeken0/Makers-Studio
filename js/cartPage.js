78b72b41bdce13666078e09e94810d70860ad33b
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2021-12-16 14:11:28 +0100

    added a function that display a message if the cart is empty

--- js/cartPage.js
+++ js/cartPage.js
@@ -13,24 +13,37 @@ for (let i = 0; i < data.length; i++) {
   $${totalAmount}.00
   `;
 }
-document.querySelector(".cart-mid").innerHTML = `
-`;
+
+if (data.length === 0) {
+  document.querySelector(".cart-mid").innerHTML = `
+    <div class="cart-mid-single">
+          <div class="cart-mid-left">
+            The cart is empty!
+          </div>
+          <div class="cart-mid-center">
+            <input type="text" aria-label="Quantity" value="-" />
+          </div>
+          <div class="cart-mid-right">$0.00</div>
+    </div>
+  `;
+}
 data.forEach((artwork) => {
   document.querySelector(".cart-mid").innerHTML += `
-    <div class="cart-mid-left">
-    <i class="fas fa-times" data-id="${artwork.id}" data-image="${artwork.Image_url}" data-name="${artwork.Title}" data-price="${artwork.Price}"></i>
-    <a href="productDetailsPage.html?id=${artwork.id}">
-    <img src="${artwork.image}" alt="an image of a painting">
-    </a>
-    <a href="productDetailsPage.html?id=${artwork.id}">
-    <h2>${artwork.name}</h2>
-    </a>
-    </div>
-    <div class="cart-mid-center">
-    <input type="text" aria-label="Quantity" value="-" />
-    </div>
-    <div class="cart-mid-right">$${artwork.price}.00</div>
-    </div>
+      <div class="cart-mid-single">
+        <div class="cart-mid-left">
+          <i class="fas fa-times" data-id="${artwork.id}" data-image="${artwork.Image_url}" data-name="${artwork.Title}" data-price="${artwork.Price}"></i>
+          <a href="productDetailsPage.html?id=${artwork.id}">
+            <img src="${artwork.image}" alt="an image of a painting">
+          </a>
+          <a href="productDetailsPage.html?id=${artwork.id}">
+            <h2>${artwork.name}</h2>
+          </a>
+        </div>
+        <div class="cart-mid-center">
+          <input type="text" aria-label="Quantity" value="-" />
+        </div>
+        <div class="cart-mid-right">$${artwork.price}.00</div>
+      </div>
     `;
 });
 
@@ -66,5 +79,5 @@ document.querySelector(".cart-btn").onclick = () => {
   alert("alert-success", "The art piece has been purchased successfully");
   setTimeout(() => {
     window.location = "index.html";
-  }, 5000);
+  }, 4000);
 };
