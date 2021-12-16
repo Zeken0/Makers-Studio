8b15cffd49af0147297e41855d569926e0f19e2c
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2021-12-16 13:18:59 +0100

    Delete configs.js

--- js/configs/configs.js
+++
@@ -1,8 +0,0 @@
-export const BASE_URL = `https://makers-studio.herokuapp.com`;
-
-export const headers = {
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-  },
-};
